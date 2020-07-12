c960277a6a49771af751a5cc060d41f51886030c
Rafael Saback <rafaelsaback@gmail.com>
2020-07-12 13:28:38 +0200

    Reduce dog's image for small screens

--- src/components/dog-image.tsx
+++ src/components/dog-image.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import './dog-image.scss';
+import { useViewportSize } from '../use-viewport-size';
 
 export const DogImage: React.FC = () => {
+  const [vpWidth] = useViewportSize();
+
+  const imgWidth = vpWidth > 350 ? '190' : '137';
+  const imgHeight = vpWidth > 350 ? '162' : '117';
+
   return (
     <div className="img-container">
       <img
         src="https://partnersontheroad.com/wp-content/uploads/2020/07/cute-dog.jpg"
-        width="190"
-        height="162"
+        width={imgWidth}
+        height={imgHeight}
       />
     </div>
   );
